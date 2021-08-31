12d46b6dc6a5bca9cb16ba9c73e2906e7f41fc4d
hzx <657070781@qq.com>
2021-08-31 20:39:08 +0800

    首页布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,42 @@
 <template>
-  <h1>home</h1>
-  <router-view></router-view>
+  <div class="basic-layout">
+    <div class="nav-side"></div>
+    <div class="content-right">
+      <div class="nav-top"></div>
+      <div class="wrapper">
+        <div class="main">
+          <router-view></router-view>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script setup>
 </script>
 
-<style scoped>
+<style lang='scss' scoped>
+.basic-layout{
+  position: relative;
+  .nav-side{
+    position: fixed;
+    width: 200px;
+    background: #001529;
+    height: 100vh;
+  }
+  .content-right{
+    margin-left: 200px;
+    .nav-top{
+      height: 50px;
+    }
+    .wrapper{
+      padding: 20px;
+      background: #eef0f3;
+      height: calc(100vh - 50px);
+      .main{
+        background-color: #fff;
+        height: 100%;
+      }
+    }
+  }
+}
 </style>
